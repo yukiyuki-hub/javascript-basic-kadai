db0d7ec6e64ff3e2a1273aa8640f4e0971dbbe9e
yukiyuki-hub <111584652+yukiyuki-hub@users.noreply.github.com>
2026-07-28 19:42:57 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -2,12 +2,13 @@
 
 let num = 15
 
-if (num % 3 == 0) {
-  console.log('3の倍数です');
+if (num % 15 == 0 && num % 5 == 0) {
+  console.log('3と5の倍数です');
 } else if (num % 5 == 0) {
   console.log('5の倍数です');
-} else if (num % 15 == 0 && num % 5 == 0) {
-  console.log('3と5の倍数です');
+} else if (num % 3 == 0) {
+  console.log('3の倍数です');
 } else {
   console.log(num);
 }
+
