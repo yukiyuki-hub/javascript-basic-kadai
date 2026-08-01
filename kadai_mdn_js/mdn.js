a260c471d4f9c1b29b34b91463f3e8af62d32899
yukiyuki-hub <111584652+yukiyuki-hub@users.noreply.github.com>
2026-08-01 23:53:34 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const today = new Date();
+
+today.getFullYear()
+today.getMonth()
+today.getDate()
+
+console.log(today.getFullYear()+"年"+
+            (today.getMonth()+1) +"月"+
+            today.getDate()+"日");
